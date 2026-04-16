a97f265d40cfdd1f7b85d539d461cfdf9683f65e
aathi1412 <itsmeaathi@gmail.com>
2026-04-16 21:30:24 +0530

    added background color for task item list

--- src/components/TodoInputList.jsx
+++ src/components/TodoInputList.jsx
@@ -3,10 +3,17 @@ export function TodoInputList({ task }){
         <>
             {task.map((taskItem, index) => {
                 return(
-                    <div key={index} className="grid grid-cols-3 gap-x-2 items-center justify-items-center">
-                        <input className="" type="checkbox" />
+                    <div 
+                    key={index} 
+                    className="grid grid-cols-3 gap-x-2 items-center justify-items-center bg-indigo-500 ml-25 mr-25 p-5 rounded-2xl">
+                        
+                        <input type="checkbox" className="w-5 h-5 accent-blue-600 " />
+
                         <span className="">{taskItem}</span>
-                        <button className=" bg-red-600">delete</button>
+
+                        <button className=" bg-red-500 border px-3 rounded-xl">
+                            delete
+                        </button>
                     </div>
                 )
             })}
